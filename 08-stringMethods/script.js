7b450dbc7a371bbfc7443c76d872c02092ae2c0b
Harry-ai-77 <ifadil728@gmail.com>
2026-07-26 16:15:25 +0700

    feat: add string methods

---
+++ 08-stringMethods/script.js
@@ -0,0 +1,29 @@
+let userName = "Fadhil Ibnu Adhari ";
+
+// console.log(userName.charAt("0")); // Menemukan huruf hanya dengan memasukkan angka (index), dimulai dari 0
+
+// console.log(userName.indexOf("F")); // Menemukan nomor urut dari suatu huruf (index), dimulai dari 0
+
+// console.log(userName.lastIndexOf("i")); // Menemukan nomor urut dari suatu huruf yg paling akhir (index), dimulai dari 0
+
+// console.log(userName.length); // Mengetahui jumlah karkter didalam variabel string
+
+// console.log(userName.trim()); // Menghapus spasi didepan dan/atau dibelakang variabel string
+
+// console.log(userName.toUpperCase()) // Membuat semua karakter menjadi kapital
+
+// console.log(userName.toLocaleLowerCase()) // Membuat semua karakter menjadi huruf kecil
+
+// console.log(userName.repeat(3)); // Membuat variabel menjadi berulang sesuai angka yang kita masukkan di tanda kurung
+
+// console.log(userName.startsWith("F")); // Menghasilkan boolean, untuk mengecek apakah suatu variabel diawali suatu karakter
+
+// console.log(userName.endsWith("h")); // Menghasilkan boolean, untuk mengecek apakah suatu variabel diakhiri suatu karakter
+
+// console.log(userName.includes(" ")); // Menghasilkan boolean, untuk mengecek apakah suatu variabel memiliki suatu karakter
+
+// console.log(userName.replaceAll(" ", "-")) // Mengganti suatu karakter dengan karakter lain (karakter apa, diganti dengan karakter apa)
+
+// console.log(userName.padStart(20, "h")); // Mengatur panjang suatu string dengan menambahkan elemen yang diinginkan diawal
+
+console.log(userName.padEnd(20, "h")); // Mengatur panjang suatu string dengan menambahkan elemen yang diinginkan diakhir
